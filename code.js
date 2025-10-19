181ed05b7f141abc08c8d2080c011fca0aa050ff
Damir <dambul2809@mail.ru>
2025-10-19 06:34:33 +0000

    Refactor extension and update methods
    Still will not work cuz of CORS issues

--- code.js
+++ code.js
@@ -1,77 +1,75 @@
 // Name: ScratchStats
-// ID: ApiStatsDash
-// Description: This extension adds modal dialog boxes.
-// By: scratch_craft_2 <https://scratch.mit.edu/users/scratch_craft_2/>
-// License: MPL-2.0
+// ID: scratchcraft2ScratchStats
+// Description: Interact with Scratch API directly in Dash!
+// By: scratch_craft_2 <https://scratch.mit.edu/users/scratch_craft_2/>,
+// Den4ik-12 <https://scratch.mit.edu/users/Den4ik-12/>,
+// damir2809 <https://scratch.mit.edu/users/damir2809/>
 
 (function (Scratch) {
     "use strict";
 
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error("This Extension must run unsandboxed");
-  }
+    if (!Scratch.extensions.unsandboxed) {
+        throw new Error("This extension must run unsandboxed!");
+    }
 
     class ScratchStats {
-      getInfo() {
-        return {
-          id: "ApiStatsDash",
-          name: "ScratchStats",
-          color1: "#ffba70",
-          blocks: [
-            {
-              opcode: "messages",
-              blockType: Scratch.BlockType.REPORTER,
-              text: "alert [textia]",
-              arguments: {
-                username: {
-                  defaultValue: "scratch_craft_2",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-          {
-              opcode: "Follow",
-              blockType: Scratch.BlockType.BOOLEAN,
-              text: "Пользователь [usernameget] Подписан на пользователя [usernamefollow] ?",
-              arguments: {
-                usernameget: {
-                  defaultValue: "scratch_craft_2",
-                  type: Scratch.ArgumentType.STRING,
-                },
-                  usernamefollow: {
-                  defaultValue: "Den4ik-12",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           }
-          ],
-        };
-      }
-messages(args) {
-  try {
-    return (await (await fetch(`https://api.scratch.mit.edu/users/${args.username}/messages/count`)).json()).count;
-  } catch (error) {
-    return ('Ошибка:', error);
-    throw error;
-}}
-Follow(args) {
-  return fetch(`https://api.scratch.mit.edu/users/${args.usernameget}/following/`)
-    .then(response => {
-      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-      return response.json();
-    })
-    .then(data => Array.isArray(data)
-      ? data.some(user =>
-          user.hasOwnProperty('username') &&
-          user.username.toLowerCase() === args.usernamefollow.toLowerCase()
-        )
-      : (() => { throw new Error('Expected an array of users in the response'); })()
-    )
-    .catch(error => {
-      console.error('Error checking following list:', error);
-      return false;
-    });
-}
+        getInfo() {
+            return {
+                id: "scratchcraft2ScratchStats",
+                name: "ScratchStats",
+                color1: "#ffba70",
+                blocks: [
+                    {
+                        opcode: "getMessages",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "messages number of user [USERNAME]",
+                        arguments: {
+                            USERNAME: {
+                                defaultValue: "scratch_craft_2",
+                                type: Scratch.ArgumentType.STRING,
+                            },
+                        },
+                    },
+                    {
+                        opcode: "isFollows",
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: "user [USERNAME1] follows user [USERNAME2]?",
+                        arguments: {
+                            USERNAME1: {
+                                defaultValue: "scratch_craft_2",
+                                type: Scratch.ArgumentType.STRING,
+                            },
+                            USERNAME2: {
+                                defaultValue: "Den4ik-12",
+                                type: Scratch.ArgumentType.STRING,
+                            },
+                        },
+                    }
+                ],
+            };
+        }
+        async getMessages(args) {
+            try {
+                return await fetch(`https://api.scratch.mit.edu/users/${args.USERNAME}/messages/count`).json().count;
+            } catch (_) {return 0};
+        }
+        async isFollows(args) {
+            return await fetch(`https://api.scratch.mit.edu/users/${args.USERNAME1}/following/`)
+                .then(response => {
+                    if (!response.ok) return false;
+                    return response.json();
+                })
+                .then(data => {
+                    if (Array.isArray(data)) {
+                        return data.some(user =>
+                            user.hasOwnProperty('username') &&
+                            user.username.toLowerCase() === args.USERNAME2.toLowerCase()
+                        );
+                    // Expected an array of users in the response
+                    } else return false;
+                })
+                .catch(_ => {return false});
+        }
     }
-    Scratch.extensions.register(new modals());
-  })(Scratch);
+    Scratch.extensions.register(new ScratchStats());
+})(Scratch);
